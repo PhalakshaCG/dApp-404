7e88b75cf819b93c2b45c591c081fd8dc76fb7c9
Phalaksha C G <phalakshacg01@gmail.com>
2022-10-23 21:06:41 +0530

    Solidity init

---
+++ smart-contract/contracts/checkmate.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Transactions {
+    uint256 counter;
+
+    event payRevenue(address from, address to, uint amount, string comment); 
+}
