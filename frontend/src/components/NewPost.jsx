7c729d530492cd550bef84aec066b64716c29198
Abhay H Kashyap <abhayastro1004@gmail.com>
2022-10-24 18:06:05 +0530

    Style Dropdown

--- frontend/src/components/NewPost.jsx
+++ frontend/src/components/NewPost.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import open from "../assets/open.svg";
 import Multiselect from "multiselect-react-dropdown";
+import arrow from "../assets/arrow.png";
 
 function NewPost() {
   const plainArray = [
@@ -10,6 +11,20 @@ function NewPost() {
     "Option 4",
     "Option 5",
   ];
+  var dropdown_style = {
+    multiselectContainer: {},
+    optionContainer: { backgroundColor: "#D1F5FF" },
+    chips: {},
+    searchBox: {
+      backgroundColor: "#D1F5FF",
+      border: "none",
+      borderRadius: "69px",
+      paddingLeft: "1rem",
+      paddingRight: "1rem",
+    },
+    inputField: {},
+    option: { color: "black" },
+  };
   return (
     <div className="border-2 rounded-[69px] pt-10 pl-[6rem] pr-[4rem] pb-[2rem] mb-7">
       <div className="content flex flex-col">
@@ -33,8 +48,19 @@ function NewPost() {
           <div className="pl-3">
             <img className="w-6" src={open} alt="" />
           </div>
-          <div className="tags">
-            <Multiselect showArrow options={plainArray} isObject={false} />
+          <div className="tags mx-7">
+            <Multiselect
+              className=""
+              showArrow
+              options={plainArray}
+              isObject={false}
+              placeholder="Add Tags"
+              style={dropdown_style}
+              avoidHighlightFirstOption={true}
+              closeOnSelect={false}
+              hidePlaceholder={true}
+              // customArrow={true}
+            />
           </div>
           <div className="submit">
             <button className="bg-[#D1F5FF] rounded-[69px] px-5 py-2 text-[#E63A0B] font-bold">
