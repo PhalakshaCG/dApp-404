26c64d124bfe953308c2cb1a035aefb888aa1e08
Abhay H Kashyap <abhayastro1004@gmail.com>
2022-12-06 12:50:41 +0530

    Add Mobile Support with Wallet Connect

---
+++ checkmate-mobile/App.js
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useWalletConnect } from '@walletconnect/react-native-dapp'
+
+export default function App() {
+  const connector = useWalletConnect();
+
+  const connectWallet = React.useCallback(() => {
+    return connector.connect();
+  }, [connector]);
+
+  const killSession = React.useCallback(() => {
+    return connector.killSession();
+  }, [connector]);
+  return (
+    <View style={styles.container}>
+      <Text>Tab One</Text>
+      {!connector.connected && (
+        <TouchableOpacity onPress={connectWallet}>
+          <Text>Connect a Wallet</Text>
+        </TouchableOpacity>
+      )}
+      {!!connector.connected && (
+        <>
+          <Text>{shortenAddress(connector.accounts[0])}</Text>
+          <TouchableOpacity onPress={killSession}>
+            <Text>Log out</Text>
+          </TouchableOpacity>
+        </>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
